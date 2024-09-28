1063ce16a7f9b142985aac53df653ea533e05278
KazuProg <91452566+KazuProg@users.noreply.github.com>
2024-09-29 02:04:30 +0900

    Feature: Pause YouTube-Player when Video loaded to deck

--- extension/js/script.js
+++ extension/js/script.js
@@ -3,6 +3,7 @@ if (location.origin == "https://www.youtube.com") {
    * YouTube-VJ YouTube
    */
   var currentUrl = null;
+  var currentVideoId = null;
   const observer = new MutationObserver(function (mutations) {
     mutations.forEach(function (mutation) {
       if (window.location.href === currentUrl) {
@@ -17,6 +18,7 @@ if (location.origin == "https://www.youtube.com") {
 
       const params = new URLSearchParams(window.location.search);
       const videoId = params.get("v");
+      currentVideoId = videoId;
 
       chrome.storage.local.set({ videoId }).then(() => {
         console.log(`YTVJ:E Set current videoId:${videoId}`);
@@ -25,12 +27,23 @@ if (location.origin == "https://www.youtube.com") {
   });
 
   observer.observe(document, { subtree: true, childList: true });
+
+  chrome.storage.onChanged.addListener((changes, namespace) => {
+    if (changes.loadedId) {
+      if (changes.loadedId.newValue === currentVideoId) {
+        document.querySelector("video").pause();
+      }
+    }
+  });
 } else {
   /**
    * YouTube-VJ Controller
    */
   const relayElement = document.querySelector("#videoId");
   console.log(relayElement);
+
+  // Reset storage
+  chrome.storage.local.set({ loadedId: null });
 
   chrome.storage.local.get("videoId", function (items) {
     console.log("YTVJ:E Read storage(videoId)");
@@ -43,5 +56,19 @@ if (location.origin == "https://www.youtube.com") {
       console.log("YTVJ:E Read storage(videoId)");
       relayElement.value = changes.videoId.newValue;
     }
+  });
+
+  console.log("YTVJ:E set MutationObserver(loadedId)");
+  const element = document.querySelector("#loadedId");
+  const observer = new MutationObserver(() => {
+    const loadedId = element.value;
+    chrome.storage.local.set({ loadedId }).then(() => {
+      console.log(`YTVJ:E loadedId=${loadedId} >Storage`);
+    });
+  });
+  observer.observe(element, {
+    attributes: true,
+    childList: true,
+    characterData: true,
   });
 }
