86230fba9e5972286bc191ee193fcbcfa666c9fe
KazuProg <91452566+KazuProg@users.noreply.github.com>
2024-09-30 03:46:14 +0900

    Change: Release and Development URL

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const btn = document.querySelector("#open_windows");
   btn.addEventListener("click", () => {
     chrome.windows.create({
-      url: "https://srv-rpi4-main.kazuprog.work/youtube-vj/controller.html",
+      url: "https://kazuprog.work/release/youtube-vj/controller",
       type: "popup",
       width: 960,
       height: 540,
